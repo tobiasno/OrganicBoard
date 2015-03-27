5a4d0bf14f7baa5dda05cf37d33eabdd89f7cc53
Tobias Nolte <tobias@abygr.com>
2015-03-27 03:51:05 +0000

    vergessen zu adden

---
+++ www/class.cookiemonster.php
@@ -0,0 +1,34 @@
+<?php
+  require_once ("class.clock.php");
+
+  class CookieMonster {
+    private $clock;
+    private $cookie_name = "OrganicBoard";
+    private $cookie_life = "30"; // in days
+    private $domain = ".mndcntrl.com";
+
+    public function __construct () {
+      $this -> clock = new Clock ();
+    }
+
+    /*
+     * setMyCookie
+     *
+     * @param none
+     * @return null
+     */
+    public function setMyCookie () {
+      setcookie ($this -> cookie_name, $this -> clock -> getTimestamp (), strtotime ('+'.$this -> cookie_life.' days'), "/", $this -> domain);
+    }
+
+    /*
+     * getTimeSinceLastVisit
+     *
+     * @param $cookie : Array
+     * @return Int, in seconds
+     */
+    public function getTimeSinceLastVisit ($cookie) {
+      return $this -> clock -> getDifference ($cookie[$this -> cookie_name], $this -> clock -> getTimestamp ());
+    }
+  }
+?>
