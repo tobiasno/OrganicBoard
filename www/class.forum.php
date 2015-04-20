cdc26d5964537c4c8a585d33f0f7602d3a9bb72b
Tobias Nolte <tobias@abygr.com>
2015-04-20 21:10:52 +0000

    documentation for closeTags and allow del tag

--- www/class.forum.php
+++ www/class.forum.php
@@ -341,12 +341,18 @@
      */
     private function enhanceContent($text){
           $text = preg_replace('!(((f|ht)tp(s)?://)[-a-zA-Zа-яА-Я()0-9@:%_+.~#?&;//=]+)!i', '<a href="$1" target="_blank">$1</a>', $text);
-          $text = preg_replace('#&lt;(/?(?:i|b|u|ul|li|ol))&gt;#', '<\1>', $text);
+          $text = preg_replace('#&lt;(/?(?:i|b|u|ul|li|ol|del))&gt;#', '<\1>', $text);
           $text = $this -> closeTags ($text);
           $text = nl2br ($text);
           return $text;
     }
 
+    /**
+     * Closes HTML tags at the end of the content that were left open
+     *
+     * @param string $html
+     * @return string
+     */
     private function closeTags ($html) {
       preg_match_all ('#<([a-z]+)(?: .*)?(?<![/|/ ])>#iU', $html, $result);
       $opened_tags = $result[1];
