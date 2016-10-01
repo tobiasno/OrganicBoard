d36cb354ba766bded1fa3fbaa2c7ec0648928b72
Tobias Nolte <tobias@abygr.com>
2016-10-01 18:18:06 +0000

    set purge interval to 12h

--- www/common/php/lib/class.forum.php
+++ www/common/php/lib/class.forum.php
@@ -18,7 +18,7 @@
     private $posts_on_frontpage = 100; // Number of posts shown on frontpage
     private $replies_on_frontpage = 5; // Number of replies shon in preview on frontpage
     // Configuration for purging unwanted posts
-    private $purge_interval = 1440; // in minutes
+    private $purge_interval = 720; // in minutes
     private $purge_threshold = 0.00005;
     // Configuration of tpoics behaviour
     private $index_topics_interval = 120; // in minutes
